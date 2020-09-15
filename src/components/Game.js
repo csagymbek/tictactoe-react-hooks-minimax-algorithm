6f7b20049419e286a30befbfebb20d0c44c3f21c
Serik Sagymbek <csagymbek@gmail.com>
2020-09-15 12:19:17 -0700

    eighth commit

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,8 +1,8 @@
-import React, {useState} from 'react';
+import React, {useState} from 'react'
 import styled from "styled-components";
 import {DIMS, SQUARE_DIMS, DRAW, PLAYER_X, PLAYER_O} from "../constants";
 
-export default function Game(){
+export default function Game() {
     const [squares, setSquares] = useState(new Array(DIMS ** 2).fill(null));
     const [players, setPlayers] = useState({
         human: PLAYER_X,
@@ -11,16 +11,23 @@ export default function Game(){
 
     const move = (index, player) => {
         setSquares(squares => {
-            const squaresCopy = squares.concat();
-            console.log(squaresCopy);
+            const squaresCopy = [...squares];
             squaresCopy[index] = player;
             return squaresCopy;
         });
     }
 
+    const humanMove = index => {
+        if(!squares[index]){
+            move(index, players.human);
+        }
+    }
+
+    
+
     return (
         <div>
-
+            
         </div>
     )
 }
